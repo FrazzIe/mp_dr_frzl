1ee7750a821b45abc6a1b042c7cb30343e166adc
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-09-17 18:15:24 +0100

    auto add iwi images to iwd

--- iwd.bat
+++ iwd.bat
@@ -11,4 +11,17 @@ IF EXIST "%create_dir%%map_name%" rmdir /S /Q "%create_dir%%map_name%"
 mkdir "%create_dir%%map_name%"
 cd "%create_dir%%map_name%"
 
+FOR /F "tokens=1-2* delims=," %%A IN (%zone_src%\%map_name%.csv) DO (
+ IF "%%~A" == "image" (
+  IF EXIST "%image_dir%\%%~B.iwi" (
+   IF NOT EXIST "%create_dir%%map_name%\images" (
+    mkdir "%create_dir%%map_name%\images"
+    cd "%create_dir%%map_name%\images"
+   )
+
+   robocopy "%image_dir%" "%create_dir%%map_name%\images" %%~B.iwi
+  )  
+ )
+)
+
 pause
