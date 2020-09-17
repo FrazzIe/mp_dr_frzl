7afaf6cb6b15ca20b18d5166b0e3fa0119800cae
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-09-17 18:32:59 +0100

    remove trialing backslash for directories, use robocopy instead of copy

--- build.bat
+++ build.bat
@@ -1,41 +1,37 @@
+@echo off
 
 set /p game_dir=<game_directory.txt
 set /p map_name=<map_name.txt
 set tools_dir=%game_dir%\bin\CoD4CompileTools
-set map_dir=%game_dir%\map_source\
-set bsp_dir=%game_dir%\raw\maps\mp\
-set git_map_dir=%~dp0map_source\
-set git_bsp_dir=%~dp0raw\maps\mp\
+set map_dir=%game_dir%\map_source
+set bsp_dir=%game_dir%\raw\maps\mp
+set git_map_dir=%~dp0map_source
+set git_bsp_dir=%~dp0raw\maps\mp
 cd "%tools_dir%"
 
-IF EXIST "%map_dir%%map_name%.map" del "%map_dir%%map_name%.map"
-IF EXIST "%map_dir%%map_name%.grid" del "%map_dir%%map_name%.grid"
-IF EXIST "%map_dir%%map_name%.lin" del "%map_dir%%map_name%.lin"
+IF EXIST "%map_dir%\%map_name%.map" del "%map_dir%\%map_name%.map"
+IF EXIST "%map_dir%\%map_name%.grid" del "%map_dir%\%map_name%.grid"
+IF EXIST "%map_dir%\%map_name%.lin" del "%map_dir%\%map_name%.lin"
 
-IF EXIST "%bsp_dir%%map_name%.d3dbsp" del "%bsp_dir%%map_name%.d3dbsp"
-IF EXIST "%bsp_dir%%map_name%.gsc" del "%bsp_dir%%map_name%.gsc"
+IF EXIST "%bsp_dir%\%map_name%.d3dbsp" del "%bsp_dir%\%map_name%.d3dbsp"
+IF EXIST "%bsp_dir%\%map_name%.gsc" del "%bsp_dir%\%map_name%.gsc"
 
-copy "%git_map_dir%%map_name%.map" "%map_dir%%map_name%.map"
-copy "%git_map_dir%%map_name%.grid" "%map_dir%%map_name%.grid"
-copy "%git_map_dir%%map_name%.lin" "%map_dir%%map_name%.lin"
+robocopy "%git_map_dir%" "%map_dir%" %map_name%.map %map_name%.grid %map_name%.lin
+robocopy "%git_bsp_dir%" "%bsp_dir%" %map_name%.d3dbsp %map_name%.gsc
 
-copy "%git_bsp_dir%%map_name%.d3dbsp" "%bsp_dir%%map_name%.d3dbsp"
-copy "%git_bsp_dir%%map_name%.gsc" "%bsp_dir%%map_name%.gsc"
-
-CALL "%tools_dir%\cod4compiletools_compilebsp.bat" "%bsp_dir%" "%map_dir%" "%game_dir%\" %map_name% - -extra 1 1 1
-
+CALL "%tools_dir%\cod4compiletools_compilebsp.bat" "%bsp_dir%\" "%map_dir%\" "%game_dir%\" %map_name% - -extra 1 1 1
 CALL "%tools_dir%\cod4compiletools_reflections.bat" "%game_dir%\" %map_name% 1
 CALL "%~dp0\ff.bat"
 
-IF EXIST "%map_dir%%map_name%.map" del "%map_dir%%map_name%.map"
-IF EXIST "%map_dir%%map_name%.grid" del "%map_dir%%map_name%.grid"
-IF EXIST "%map_dir%%map_name%.lin" del "%map_dir%%map_name%.lin"
+IF EXIST "%map_dir%\%map_name%.map" del "%map_dir%\%map_name%.map"
+IF EXIST "%map_dir%\%map_name%.grid" del "%map_dir%\%map_name%.grid"
+IF EXIST "%map_dir%\%map_name%.lin" del "%map_dir%\%map_name%.lin"
 
-IF EXIST "%bsp_dir%%map_name%.d3dbsp" (
- del "%git_bsp_dir%%map_name%.d3dbsp"
- copy "%bsp_dir%%map_name%.d3dbsp" "%git_bsp_dir%%map_name%.d3dbsp"
- del "%bsp_dir%%map_name%.d3dbsp"
+IF EXIST "%bsp_dir%\%map_name%.d3dbsp" (
+ del "%git_bsp_dir%\%map_name%.d3dbsp"
+ copy "%bsp_dir%\%map_name%.d3dbsp" "%git_bsp_dir%\%map_name%.d3dbsp"
+ del "%bsp_dir%\%map_name%.d3dbsp"
 )
-IF EXIST "%bsp_dir%%map_name%.gsc" del "%bsp_dir%%map_name%.gsc"
+IF EXIST "%bsp_dir%\%map_name%.gsc" del "%bsp_dir%\%map_name%.gsc"
 
 pause
