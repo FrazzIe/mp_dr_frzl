93ee981d4500ad7a1e75f3f0072fa3c0c9d9cfce
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-09-19 19:07:07 +0100

    move csv creation, copying to build.bat
    - Reflections apparently use this file?

--- build.bat
+++ build.bat
@@ -5,8 +5,10 @@ set /p map_name=<map_name.txt
 set tools_dir=%game_dir%\bin\CoD4CompileTools
 set map_dir=%game_dir%\map_source
 set bsp_dir=%game_dir%\raw\maps\mp
+set zone_src_dir=%game_dir%\zone_source
 set git_map_dir=%~dp0map_source
 set git_bsp_dir=%~dp0raw\maps\mp
+set git_zone_src_dir=%~dp0zone_source\english\assetinfo
 cd "%tools_dir%"
 
 IF EXIST "%map_dir%\%map_name%.map" del "%map_dir%\%map_name%.map"
@@ -16,8 +18,32 @@ IF EXIST "%map_dir%\%map_name%.lin" del "%map_dir%\%map_name%.lin"
 IF EXIST "%bsp_dir%\%map_name%.d3dbsp" del "%bsp_dir%\%map_name%.d3dbsp"
 IF EXIST "%bsp_dir%\%map_name%.gsc" del "%bsp_dir%\%map_name%.gsc"
 
+IF EXIST "%zone_src_dir%\%map_name%.csv" del "%zone_src_dir%\%map_name%.csv"
+IF EXIST "%zone_src_dir%\%map_name%_load.csv" del "%zone_src_dir%\%map_name%_load.csv"
+
+IF NOT EXIST %git_zone_src_dir%\%map_name%.csv (
+ echo ignore,code_post_gfx_mp> %git_zone_src_dir%\%map_name%.csv
+ echo ignore,common_mp>> %git_zone_src_dir%\%map_name%.csv
+ echo ignore,localized_code_post_gfx_mp>> %git_zone_src_dir%\%map_name%.csv
+ echo ignore,localized_common_mp>> %git_zone_src_dir%\%map_name%.csv
+ echo col_map_mp,maps/mp/%map_name%.d3dbsp>> %git_zone_src_dir%\%map_name%.csv
+ echo rawfile,maps/mp/%map_name%.gsc>> %git_zone_src_dir%\%map_name%.csv
+ echo impactfx,%map_name%>> %git_zone_src_dir%\%map_name%.csv
+ echo sound,common,%map_name%,!all_mp>> %git_zone_src_dir%\%map_name%.csv
+ echo sound,generic,%map_name%,!all_mp>> %git_zone_src_dir%\%map_name%.csv
+ echo sound,voiceovers,%map_name%,!all_mp">> %git_zone_src_dir%\%map_name%.csv
+ echo sound,multiplayer,%map_name%,!all_mp>> %git_zone_src_dir%\%map_name%.csv
+
+ echo ignore,code_post_gfx_mp> %git_zone_src_dir%\%map_name%_load.csv
+ echo ignore,common_mp>> %git_zone_src_dir%\%map_name%_load.csv
+ echo ignore,localized_code_post_gfx_mp>> %git_zone_src_dir%\%map_name%_load.csv
+ echo ignore,localized_common_mp>> %git_zone_src_dir%\%map_name%_load.csv
+ echo ui_map,maps/%map_name%>> %git_zone_src_dir%\%map_name%_load.csv
+)
+
 robocopy "%git_map_dir%" "%map_dir%" %map_name%.map %map_name%.grid %map_name%.lin
 robocopy "%git_bsp_dir%" "%bsp_dir%" %map_name%.d3dbsp %map_name%.gsc
+robocopy "%git_zone_src_dir%" "%zone_src_dir%" %map_name%.csv %map_name%_load.csv
 
 CALL "%tools_dir%\cod4compiletools_compilebsp.bat" "%bsp_dir%\" "%map_dir%\" "%game_dir%\" %map_name% - -extra 1 1 1
 CALL "%tools_dir%\cod4compiletools_reflections.bat" "%game_dir%\" %map_name% 1
--- ff.bat
+++ ff.bat
@@ -8,27 +8,6 @@ set zone_dir=%game_dir%\zone\english
 set git_zone_src_dir=%~dp0zone_source\english\assetinfo
 set git_zone_dir=%~dp0zone\english
 
-IF NOT EXIST %git_zone_src_dir%\%map_name%.csv (
- echo work
- echo ignore,code_post_gfx_mp> %git_zone_src_dir%\%map_name%.csv
- echo ignore,common_mp>> %git_zone_src_dir%\%map_name%.csv
- echo ignore,localized_code_post_gfx_mp>> %git_zone_src_dir%\%map_name%.csv
- echo ignore,localized_common_mp>> %git_zone_src_dir%\%map_name%.csv
- echo col_map_mp,maps/mp/%map_name%.d3dbsp>> %git_zone_src_dir%\%map_name%.csv
- echo rawfile,maps/mp/%map_name%.gsc>> %git_zone_src_dir%\%map_name%.csv
- echo impactfx,%map_name%>> %git_zone_src_dir%\%map_name%.csv
- echo sound,common,%map_name%,!all_mp>> %git_zone_src_dir%\%map_name%.csv
- echo sound,generic,%map_name%,!all_mp>> %git_zone_src_dir%\%map_name%.csv
- echo sound,voiceovers,%map_name%,!all_mp">> %git_zone_src_dir%\%map_name%.csv
- echo sound,multiplayer,%map_name%,!all_mp>> %git_zone_src_dir%\%map_name%.csv
-
- echo ignore,code_post_gfx_mp> %git_zone_src_dir%\%map_name%_load.csv
- echo ignore,common_mp>> %git_zone_src_dir%\%map_name%_load.csv
- echo ignore,localized_code_post_gfx_mp>> %git_zone_src_dir%\%map_name%_load.csv
- echo ignore,localized_common_mp>> %git_zone_src_dir%\%map_name%_load.csv
- echo ui_map,maps/%map_name%>> %git_zone_src_dir%\%map_name%_load.csv
-)
-
 IF EXIST "%zone_src_dir%\%map_name%.csv" del "%zone_src_dir%\%map_name%.csv"
 IF EXIST "%zone_src_dir%\%map_name%_load.csv" del "%zone_src_dir%\%map_name%_load.csv"
 IF EXIST "%zone_dir%\%map_name%.ff" del "%zone_dir%\%map_name%.ff"
@@ -39,16 +18,6 @@ robocopy "%git_zone_src_dir%" "%zone_src_dir%" %map_name%.csv %map_name%_load.cs
 cd "%bin_dir%"
 "%bin_dir%\linker_pc.exe" %map_name% %map_name%_load
 
-IF EXIST "%zone_src_dir%\%map_name%.csv" (
- del "%git_zone_src_dir%\%map_name%.csv"
- robocopy "%zone_src_dir%" "%git_zone_src_dir%" %map_name%.csv
- del "%zone_src_dir%\%map_name%.csv"
-)
-IF EXIST "%zone_src_dir%\%map_name%_load.csv" (
- del "%git_zone_src_dir%\%map_name%_load.csv"
- robocopy "%zone_src_dir%" "%git_zone_src_dir%" %map_name%_load.csv
- del "%zone_src_dir%\%map_name%_load.csv"
-)
 IF EXIST "%zone_dir%\%map_name%.ff" (
  del "%git_zone_dir%\%map_name%.ff"
  robocopy "%zone_dir%" "%git_zone_dir%" %map_name%.ff
