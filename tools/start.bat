e595982d3256f89d193163e42d19bba32116674a
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-10-03 18:24:19 +0100

    fix .ff files not being moved on start

--- tools/start.bat
+++ tools/start.bat
@@ -9,7 +9,7 @@ set git_bsp_dir=%~dp0..\raw\maps\mp\
 set zone_src_dir=%game_dir%\zone_source
 set zone_dir=%game_dir%\zone\english
 set git_zone_src_dir=%~dp0..\zone_source\english\assetinfo
-set git_zone_dir=%~dp0..\zone\english
+set output_dir=%~dp0..\output
 set options="+set developer 1 +set developer_script 1 +set sv_cheats 1 +set fs_game "mods/deathrun_dev" +set g_gametype deathrun +set gametype deathrun +set dr_freerun_time 9999"
 
 IF EXIST "%map_dir%%map_name%.map" del "%map_dir%%map_name%.map"
@@ -28,6 +28,6 @@ copy "%git_bsp_dir%%map_name%.d3dbsp" "%bsp_dir%%map_name%.d3dbsp"
 copy "%git_bsp_dir%%map_name%.gsc" "%bsp_dir%%map_name%.gsc"
 
 robocopy "%git_zone_src_dir%" "%zone_src_dir%" %map_name%.csv %map_name%_load.csv
-robocopy "%git_zone_dir%" "%zone_dir%" %map_name%.ff %map_name%_load.ff
+robocopy "%output_dir%" "%zone_dir%" %map_name%.ff %map_name%_load.ff
 
 CALL "%tools_dir%\cod4compiletools_runmap.bat" "%game_dir%\" %map_name% 1 %options%
