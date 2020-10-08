1b55dda861fe8e4fafae4ddae17d888098e3ca54
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-10-08 20:21:09 +0100

    remove useless entries in start bat

--- tools/start.bat
+++ tools/start.bat
@@ -12,22 +12,9 @@ set git_zone_src_dir=%~dp0..\zone_source\english\assetinfo
 set output_dir=%~dp0..\output
 set options="+set developer 1 +set developer_script 1 +set sv_cheats 1 +set fs_game "mods/deathrun_dev" +set g_gametype deathrun +set gametype deathrun +set dr_freerun_time 9999"
 
-IF EXIST "%map_dir%%map_name%.map" del "%map_dir%%map_name%.map"
-
-IF EXIST "%bsp_dir%%map_name%.d3dbsp" del "%bsp_dir%%map_name%.d3dbsp"
-IF EXIST "%bsp_dir%%map_name%.gsc" del "%bsp_dir%%map_name%.gsc"
-
-IF EXIST "%zone_src_dir%\%map_name%.csv" del "%zone_src_dir%\%map_name%.csv"
-IF EXIST "%zone_src_dir%\%map_name%_load.csv" del "%zone_src_dir%\%map_name%_load.csv"
 IF EXIST "%zone_dir%\%map_name%.ff" del "%zone_dir%\%map_name%.ff"
 IF EXIST "%zone_dir%\%map_name%_load.ff" del "%zone_dir%\%map_name%_load.ff"
 
-copy "%git_map_dir%%map_name%.map" "%map_dir%%map_name%.map"
-
-copy "%git_bsp_dir%%map_name%.d3dbsp" "%bsp_dir%%map_name%.d3dbsp"
-copy "%git_bsp_dir%%map_name%.gsc" "%bsp_dir%%map_name%.gsc"
-
-robocopy "%git_zone_src_dir%" "%zone_src_dir%" %map_name%.csv %map_name%_load.csv
 robocopy "%output_dir%" "%zone_dir%" %map_name%.ff %map_name%_load.ff
 
 CALL "%tools_dir%\cod4compiletools_runmap.bat" "%game_dir%\" %map_name% 1 %options%
